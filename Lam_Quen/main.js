54e3f9db4cbb6005bf5d71edeaa14744429e5769
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2023-03-14 16:42:19 +0700

    Create main.js

---
+++ Lam_Quen/main.js
@@ -0,0 +1,26 @@
+//console.log("Trương Văn Hoàng"); //hàm dùng để in giá trị của một biến ra ngoài ô Console của Inspect Element
+//alert("Văn Hoàng"); //hiển thi hộp thoại cảnh báo 
+var hoTen = "Trương Văn Hoàng";
+var ngaySinh = "11/08/2002";
+var nganhHoc = "Phát triển phần mềm";
+console.log(hoTen);
+console.log(ngaySinh);
+console.log(nganhHoc);
+// khai báo năm sinh, kiểm tra xem năm sinh đó chẵn hay lẻ
+// toan tu so sanh === la so sanh kieu du lieu
+var namSinh = 2000;
+var le = "la nam le!";
+var chan = "la nam chan";
+if(namSinh % 2 == 0)
+{
+    console.log(namSinh, chan);
+}
+else 
+{
+    console.log(namSinh, le);
+}
+var namHienTai = 2023 - namSinh;
+console.log("So tuoi hien tai:", namHienTai);
+//alert(hoTen);
+//alert(ngaySinh);
+//alert(nganhHoc);
