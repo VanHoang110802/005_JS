7526108132d6a7bdc2dac0d96c9651fb259179ff
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2023-03-22 11:43:12 +0700

    Create main.js

---
+++ Buoi6/main.js
@@ -0,0 +1,39 @@
+// // let a = 11;
+// // let b = 08;
+// // let c = 2002;
+
+// // // tính tổng của 2 số ngẫu nhiên
+// // let tong1 = a + b;
+// // let tong2 = a + c;
+// // let tong3 = b + c;
+
+// // console.log(tong1);
+// // console.log(tong2);
+// // console.log(tong3);
+
+// // hàm: là 1 khối mã thực hiện 1 công việc cụ thể nào đó.
+
+// //4 tính chất của hàm:
+// // + hàm không tự chạy (thực thi)
+// // + hàm chỉ chạy khi được gọi đến
+// // + hàm có thể nhận được tham số
+// // + hàm có thể trả về 1 giá trị
+
+// // cách khai báo:
+
+// function TinhTong(a, b) 
+// {
+//     let ans = a + b;
+//     alert(ans);
+// }
+// //let ans = TinhTong(a, b);
+// //console.log(ans);
+
+// // function sayHello()
+// // {
+// //     alert("Hello");
+// // }
+
+
+
+let val = 100; let message = (val > 100) ? `${val} was greater than 100` : `${val} was LESS or Equal to 100`; let check = (val % 2) ? `Odd` : `Even`; check = `${val} is ${check}`; console.log(check);
