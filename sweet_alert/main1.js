2fe47d435efcd36f41ed3d8fd220cd39d125c6d1
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2023-03-29 22:26:49 +0700

    Create main1.js

---
+++ sweet_alert/main1.js
@@ -0,0 +1,13 @@
+
+swal({
+    title: "Xin chào",
+    icon: "info",
+    buttons: [{
+         text: "Cancel", 
+         value: null,
+        }, 
+        {
+            text: "Ok", 
+            value: true,
+        }],
+})
