94002bacaab18cc0e88f88956a3dc5424a202133
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2023-04-05 23:09:06 +0700

    Update buoi13.js

--- BTLT/buoi13.js
+++ BTLT/buoi13.js
@@ -1,287 +1,230 @@
+function myButtonSubmit() {
+    let arr = document.getElementsByTagName('input');
 
-// swal({
-//     title: "Hoàng đẹp trai vc",
-//     icon: "info", 
-//     buttons: [{
-//          text: "Cancel", 
-//          value: null,
-//         }, 
-//         {
-//             text: "Ok", 
-//             value: true,
-//         }],
-// })
+    // ho ten
+    let getName = arr[0].value;
 
-let arrLuuTru = ["", "" , "", "" , ""]; 
+    let thongBaoName = document.getElementsByClassName('thongBaoName');
+    let checkNumInName = false;
+    if (getName == "" || getName == null) {
+        thongBaoName[0].innerHTML = "(*)Thông tin không được để trống.";
+        return;
+    }
+    else {
+        thongBaoName[0].innerHTML = "";
+        for (let i = 0; i < getName.length; ++i) {
+            if ((getName[i] < 'A' || (getName[i] > 'Z' && getName[i] < 'a') || getName[i] > 'z') && getName[i] != ' ') {
+                checkNumInName = true;
+                break;
+            }
+        }
 
-function checkName()
-{
-    let checkTen = document.getElementById('register_name').value;
-    let thongBaoSpan = document.getElementsByClassName('thongBaoSpan');
-    let checkKiTuSo = false;
-    
-    for(let i=0;i<checkTen.length;++i)
-    {
-        if(checkTen[i] >= '0' && checkTen[i] <= '9')
+        if (checkNumInName) // co so trong chuoi ten
         {
-            checkKiTuSo = true;
-            break;
+            thongBaoName[0].innerHTML = "(*)Thông tin họ tên nhập không hợp lệ.";
+            return;
+        }
+        else {
+            thongBaoName[0].innerHTML = "";
         }
     }
-    
-    if(checkKiTuSo)
-    {
-        document.getElementById('register_name').style.borderColor = 'red';
-        thongBaoSpan[0].innerHTML = "*Nhập không hợp lệ. Xin kiểm tra lại!!";
-    }
-    else 
-    {
-        document.getElementById('register_name').style.borderColor = 'black';
-        thongBaoSpan[0].innerHTML = "";
-    }
-}
 
-function checkNumber()
-{
-    let checkTen = document.getElementById('register_number').value;
+
+    // so dien thoai
+    let getSDT = arr[1].value;
     let thongBaoSDT = document.getElementsByClassName('thongBaoSDT');
-    if(isNaN(checkTen))
-    {
-        document.getElementById('register_number').style.borderColor = 'red';
-        thongBaoSDT[0].innerHTML = "Nhập không hợp lệ. Xin kiểm tra lại!!";
+    var regexNumber = /^\d+$/;
+    var regexSDT = /((09|03|07|08|05)+([0-9]{8})\b)/g;
+
+    if (getSDT == "" || getSDT == null) {
+        thongBaoSDT[0].innerHTML = "(*)Thông tin không được để trống.";
+        return;
     }
-    else
+    else {
+        thongBaoSDT[0].innerHTML = "";
+
+    }
+    if (!regexNumber.test(getSDT))
     {
-        document.getElementById('register_number').style.borderColor = 'black';
+        thongBaoSDT[0].innerHTML = "(*)Số điện thoại nhập không hợp lệ.";
+        return;
+    }
+    else {
         thongBaoSDT[0].innerHTML = "";
     }
-}
 
-function myButtonTest()
-{
-    let arr = document.getElementsByTagName('input');
-    let name = arr[0].value;
-    let sdt = arr[1].value;
-    let mail = arr[2].value;
-    let sex = "";
-
-    //console.log(arr);
-    //console.log(name);
-    //console.log(sdt);
-    //console.log(mail);
-    if(arr[3].checked)
+    if (!regexSDT.test(getSDT)) 
     {
-        sex = arr[3].value;
-        //console.log(sex);
-    }
-    else 
-    {
-        sex = arr[4].value;
-        //console.log(sex);
-    }
-    //let vn = document.getElementById('Vietnam');
-    //console.log(vn.value);
-    if(name == "")
-    {
-        let a = document.getElementsByClassName('test');
-        a[0].innerHTML = "(*)";
-    }
-    else 
-    {
-        let a = document.getElementsByClassName('test');
-        a[0].innerHTML = "";
-    }
-    if(sdt == "")
-    {
-        let a = document.getElementsByClassName('test1');
-        a[0].innerHTML = "(*)";
-    }
-    else 
-    {
-        let a = document.getElementsByClassName('test1');
-        a[0].innerHTML = "";
-    }
-    if(mail == "")
-    {
-        let a = document.getElementsByClassName('test2');
-        a[0].innerHTML = "(*)";
-    }
-    else 
-    {
-        let a = document.getElementsByClassName('test2');
-        a[0].innerHTML = "";
-    }
-
-    let c1 = document.getElementsByClassName('label');
-    //console.log(c1[5].value);
-
-    arrLuuTru[0] = name;
-    arrLuuTru[1] = sdt;
-    arrLuuTru[2] = mail;
-    arrLuuTru[3] = sex;
-    arrLuuTru[4] = c1[5].value;
-
-    for(let i=0;i<arrLuuTru.length;++i)
-    {
-        console.log(arrLuuTru[i]);
-    }
-
-    // if(name == "" || sdt == "" || mail == "")
-    // {
-    //     //let thongBao = document.getElementsByClassName('thongBao');
-    //     swal({title: "Đăng kí thông tin không thành công", icon: "error"});
-    //     //thongBao[0].innerHTML = "Ảo thật đấy";
-    //     document.getElementsByTagName('form')[0].reset();
-    //     return;
-    // }
-    
-
-    // if(isNaN(sdt))
-    // {
-    //     swal({title: "Đăng kí sdt không thành công", icon: "error"});
-    //     document.getElementsByTagName('form')[0].reset();
-    //     return;
-    // }
-
-     if(name != "" && sdt != "" && mail != "")
-     {
-        
-       // window.location.href = "buoi13_1.html";
-      
-        swal("Đăng kí thành công!!", "", "success");
-        
-     }
-     else 
-     {
-        swal("Đăng kí không thành công!!", "Xin lưu ý: (*) là phần bắt buộc phải điền thông tin", "error");
-        document.getElementById('ketqua').innerHTML = "";
+        thongBaoSDT[0].innerHTML = "(*)Thông tin số điện thoại nhập không hợp lệ.";
         return;
-     }
+    }
+    else {
+        thongBaoSDT[0].innerHTML = "";
+    }
+
+
+    // cmt
+    let getCMT = arr[2].value;
+    let thongBaoCMT = document.getElementsByClassName('thongBaoCMT');
+    if (getCMT == "" || getCMT == null) {
+        thongBaoCMT[0].innerHTML = "(*)Thông tin không được để trống.";
+        return;
+    }
+    else {
+        thongBaoCMT[0].innerHTML = "";
+
+    }
+    if (!regexNumber.test(getCMT))
+    {
+        thongBaoCMT[0].innerHTML = "(*)Số chứng thư nhập không hợp lệ.";
+        return;
+    }
+    else {
+        if (getCMT.length < 12) {
+            thongBaoCMT[0].innerHTML = "(*)Số lượng kí tự trong chứng minh thư không hợp lệ.";
+            return;
+        }
+
+        else thongBaoCMT[0].innerHTML = "";
+    }
+
+    // email
+    let getEmail = arr[3].value;
+    
+    var regexEmail = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
+    let thongBaoEmail = document.getElementsByClassName('thongBaoEmail');
+    if (getEmail == "" || getEmail == null) {
+        thongBaoEmail[0].innerHTML = "(*)Thông tin không được để trống.";
+        return;
+    }
+    else {
+        thongBaoEmail[0].innerHTML = "";
+    }
+    if (!regexEmail.test(getEmail))
+    {
+        thongBaoEmail[0].innerHTML = "(*)Số chứng thư nhập không hợp lệ.";
+        return;
+    }
+    else {
+        thongBaoEmail[0].innerHTML = "";
+    }
+
+    // chuyen bay
+    let thongBaoChuyenBay = document.getElementsByClassName('thongBaoChuyenBay');
+    let lcChuyenBay = document.getElementsByClassName('luaChonChuyenBay');
+    let cb = lcChuyenBay[0].value;
+    
+    if (cb == "" || cb == null) {
+        thongBaoChuyenBay[0].innerHTML = "(*)Thông tin không được để trống.";
+        return;
+    }
+    else 
+    {
+        thongBaoChuyenBay[0].innerHTML = "";
+    }
+
+    // ngay thang nam sinh
+    let ntn = arr[4].value;
+    let thongBaoNTN = document.getElementsByClassName('thongBaoNTN');
+    
+    if (ntn == "" || ntn == null) {
+        thongBaoNTN[0].innerHTML = "(*)Thông tin không được để trống.";
+        return;
+    }
+    else 
+    {
+        thongBaoNTN[0].innerHTML = "";
+    }
+
+    // hanh ly
+    let hanhLy = arr[5].value;
+    
+    let thongBaoHanhLy = document.getElementsByClassName('thongBaoHanhLy');
+    if (hanhLy == "" || hanhLy == null) {
+        thongBaoHanhLy[0].innerHTML = "(*)Thông tin không được để trống.";
+        return;
+    }
+    else 
+    {
+        thongBaoHanhLy[0].innerHTML = "";
+    }
+
+    // gioi tinh
+    let sex = "";
+    let thongBaoGioiTinh = document.getElementsByClassName('thongBaoGioiTinh');
+    if(arr[6].checked)
+    {
+        sex = arr[6].value;
+    }
+    else if(arr[7].checked)
+    {
+        sex = arr[7].value
+    }
+    if (sex == "" || sex == null) {
+        thongBaoGioiTinh[0].innerHTML = "(*)Thông tin không được để trống.";
+        return;
+    }
+    else 
+    {
+        thongBaoGioiTinh[0].innerHTML = "";
+    }
+
+    // quoc tich
+    let thongBaoQuocTich = document.getElementsByClassName('thongBaoQuocTich');
+    let lcQuocTich = document.getElementsByClassName('luaChonQuocTich');
+    let qt = lcQuocTich[0].value;
+    
+    if (qt == "" || qt == null) {
+        thongBaoQuocTich[0].innerHTML = "(*)Thông tin không được để trống.";
+        return;
+    }
+    else 
+    {
+        thongBaoQuocTich[0].innerHTML = "";
+    }
+
     // Tạo 1 chuỗi chính là bảng HTML
-    let result = 
-    `<tr>
-        <td>Họ tên</td>
-        <td>${arrLuuTru[0]}</td>
-       
+    let result =
+        `<tr>
+     <td>Họ tên</td>
+     <td>${getName}</td>
+    
     </tr>
     <tr>
         <td>Số điện thọai</td>
-        <td>${arrLuuTru[1]}</td>
+        <td>${getSDT}</td>
+    </tr>
+    <tr>
+        <td>Chứng minh thư</td>
+        <td>${getCMT}</td>
     </tr>
     <tr>
         <td>Email</td>
-        <td>${arrLuuTru[2]}</td>
+        <td>${getEmail}</td>
+    </tr>
+    <tr>
+        <td>Chuyến bay</td>
+        <td>${cb}</td>
+    </tr>
+    <tr>
+        <td>Hành lý</td>
+        <td>${hanhLy}</td>
     </tr>
     <tr>
         <td>Giới tính</td>
-        <td>${arrLuuTru[3]}</td>
+        <td>${sex}</td>
     </tr>
     <tr>
         <td>Quốc tịch</td>
-        <td>${arrLuuTru[4]}</td>
-    </tr>`;
+        <td>${qt}</td>
+    </tr>
+    `;
     // in ra bảng HTML bằng innerHTML
     document.getElementById('ketqua').innerHTML = result;
-        
 }
 
 function myButtonReset()
 {
     document.getElementsByTagName('form')[0].reset();
-    document.getElementById('ketqua').innerHTML = "";
+    //document.getElementById('ketqua').innerHTML = "";
 }
-
-// function myButtonEmail() {
-//     // dùng dom để in ra giá trị nhập vào của form input
-//     let email = document.getElementById('mail');
-//     let thongBao = document.getElementsByClassName('thongBao');
-
-//     console.log(email.value);
-//     if (email.value == '') {
-//         thongBao[0].innerHTML = "Vui lòng nhập đầy đủ thông tin.";
-//         return;
-       
-//     }
-
-//     let regexEmail = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
-//     if (!regexEmail.test(email.value)) {
-//         thongBao[0].innerHTML = "Email nhập không hợp lệ. Xin kiểm tra lại!!";
-//         return;
-        
-//     }
-//     else if (regexEmail.test(email.value)) {
-//         thongBao[0].innerHTML = "";
-//         return;
-//     }
-// }
-
-
-// function myButtonSDT() {
-//     // dùng dom để in ra giá trị nhập vào của form input
-//     let SDT = document.getElementById('sdt');
-//     let thongBao1 = document.getElementsByClassName('thongBao1');
-
-//     console.log(SDT.value);
-//     if (SDT.value == '') {
-//         thongBao1[0].innerHTML = "Vui lòng nhập đầy đủ thông tin.";
-//         return;
-//     }
-
-//     let regexSDT = /((09|03|07|08|05|01)+([0-9]{8})\b)/g;
-//     if (regexSDT.test(SDT.value)) {
-//         thongBao1[0].innerHTML = "";
-//         return;
-//     }
-//     else if (!(regexSDT.test(SDT.value))) {
-//         thongBao1[0].innerHTML = "Sdt nhập không hợp lệ. Xin kiểm tra lại!!";
-//         return;
-//     }
-// }
-
-/*
-// swal("Xin Chào");
-
-// swal({
-//     title: "title",
-//     text: "Hello world!",
-// });
-// swal("Here's the title!", "...and here's the text!", "error");
-
-function myButton() {
-    let thongBao = document.getElementsByClassName('thongbao');
-    // Dùng DOM để in ra giá trị nhập vào của form input
-    let email = document.getElementById('email');
-    console.log(email.value);
-    // Validate email
-    let regexEmail = /^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i;
-    // Nếu là email thì trả về TRUE còn ko phải là email thì trả về FALSE
-    console.log("Kiểm tra email:", regexEmail.test(email.value));
-    // Validate rỗng
-    if (email.value == "" || email.value == null) {
-        thongBao[0].innerHTML = "Bạn chưa nhập email";
-        // swal("Bạn chưa nhập email", "Vui lòng nhập đầy đủ thông tin", "error");
-    } 
-    else if (!regexEmail.test(email.value)) {
-        thongBao[0].innerHTML = "Bạn đã nhập sai email. Vui lòng nhập lại.";
-        // swal("Bạn nhập sai email", "Vui lòng nhập lại thông tin", "error");
-    }
-    // Nếu nhập lại là đúng thì sẽ tắt thông báo
-    if (regexEmail.test(email.value)) {
-        thongBao[0].innerHTML = "";
-    }
-
-    let phoneNumber = document.getElementById('phoneNumber');
-    console.log(phoneNumber.value);
-    var regexPhone = /((09|03|07|08|05)+([0-9]{8})\b)/g;
-    console.log("Kiểm tra phone:", regexPhone.test(phoneNumber.value));
-    if (phoneNumber.value == "" || phoneNumber.value == null) {
-        thongBao[1].innerHTML = "Bạn chưa nhập số điện thoại";
-    } else if (!regexPhone.test(phoneNumber.value)) {
-        thongBao[1].innerHTML = "Vui lòng nhập lại số điện thoại";
-    }
-    // Nếu nhập lại là đúng thì sẽ tắt thông báo
-    if (regexPhone.test(phoneNumber.value)) {
-        thongBao[1].innerHTML = "";
-    }
-}
-*/
