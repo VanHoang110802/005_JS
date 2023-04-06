ad6e87c13e19aed30d688f22b229b9370d87e77b
Truong Van Hoang <108053955+VanHoang110802@users.noreply.github.com>
2023-04-07 06:47:19 +0700

    Create style1.js

---
+++ ASS_GD2/style/style1.js
@@ -0,0 +1,43 @@
+.slider1 {
+    background-image: url(/img/bicyclesting-c.jpg); 
+    background-repeat: no-repeat;
+    background-size: cover;
+    width: 100%;
+    height: 850px;
+    background-position: center;
+    padding-top: 100px;
+    padding-bottom: 120px;
+    
+}
+
+p{
+    margin:10px;
+    padding-bottom:10px;
+    background-size: cover;
+}
+.text-inp
+{
+    margin-left: 100px;
+    font-size: 20px;
+}
+.text-inp1
+{
+    margin-left: 100px;
+    color: aliceblue;
+    font-size: 20px;
+}
+.text-inp2
+{
+    margin-left: 110px;
+    font-size: 25px;
+}
+.text-inp3
+{
+    margin-left: 200px;
+    font-size: 25px;
+}
+.text-inp4
+{
+    margin-left: 200px;
+    font-size: 25px;
+}
